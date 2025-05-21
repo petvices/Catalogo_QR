46e42fd9d9c8c82b1671138a8fa0b766483f4a4e
petvices <petvices@gmail.com>
2025-05-20 20:08:41 -0400

    Update FacebookPixel.js

--- components/FacebookPixel.js
+++ components/FacebookPixel.js
@@ -1,6 +1,13 @@
+// components/FacebookPixel.js
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 const FacebookPixel = () => {
+  useEffect(() => {
+    // Fuerza el envío del PageView después de cargar el componente
+    window.fbq?.('track', 'PageView')
+  }, [])
+
   return (
     <>
       <Script
@@ -8,29 +15,25 @@ const FacebookPixel = () => {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '883220427263702');
-            fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '883220427263702'); // Tu ID real
+              fbq('track', 'PageView'); // Envía el evento inicial
           `,
         }}
       />
       <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
+        <img height="1" width="1" style={{ display: 'none' }} 
           src="https://www.facebook.com/tr?id=883220427263702&ev=PageView&noscript=1"
         />
       </noscript>
     </>
   )
 }
-
 export default FacebookPixel
