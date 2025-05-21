66b9c859e5e514030a18a8b60c5d2cb384d8ae98
petvices <petvices@gmail.com>
2025-05-20 20:42:28 -0400

    Update FacebookPixel.js

--- components/FacebookPixel.js
+++ components/FacebookPixel.js
@@ -1,15 +1,6 @@
-"use client"; // Añade esto al inicio
-
 import Script from 'next/script'
-import { useEffect } from 'react'
 
 const FacebookPixel = () => {
-  useEffect(() => {
-    if(typeof window !== 'undefined') {
-      window.fbq?.('track', 'PageView')
-    }
-  }, [])
-
   return (
     <>
       <Script
@@ -17,21 +8,24 @@ const FacebookPixel = () => {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s){
-              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window,document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '736085119064741');
-              fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '736085119064741');
+            fbq('track', 'PageView');
           `,
         }}
       />
       <noscript>
-        <img height="1" width="1" style={{ display: 'none' }} 
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
           src="https://www.facebook.com/tr?id=736085119064741&ev=PageView&noscript=1"
         />
       </noscript>
